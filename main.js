dce52db60535cf64f059d8daf014396745bc0020
Guilherme Gomes <93009014+Hivegui@users.noreply.github.com>
2024-10-18 11:31:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,45 @@
 const LinksSocialMedia = {
-  github: 'hivegui',
-  linkedin: 'guilhermefariasgomes',
-  facebook: 'hivegui',
-  instagram: 'hivegui',
-  twitter: 'hiveguii'
+  github: "hivegui",
+  linkedin: "guilhermefariasgomes",
+  facebook: "hivegui",
+  instagram: "hivegui",
+  twitter: "hiveguii",
+};
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute("class");
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`;
+  }
 }
 
 function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
-    const social = li.getAttribute('class')
+    const social = li.getAttribute("class");
 
-    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+    if (social === "linkedin") {
+      li.children[0].href = `https://www.linkedin.com/in/${LinksSocialMedia[social]}`;
+    } else {
+      li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`;
+    }
   }
 }
 
-changeSocialMediaLinks()
+changeSocialMediaLinks();
 
 function getGitHubProfileInfos() {
-  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`;
 
   fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      userName.textContent = data.name
-      userBio.textContent = data.bio
-      userLink.href = data.html_url
-      UserImage.src = data.avatar_url
-      userLogin.textContent = data.login
-    })
+    .then((response) => response.json())
+    .then((data) => {
+      userName.textContent = data.name;
+      userBio.textContent = data.bio;
+      userLink.href = data.html_url;
+      UserImage.src = data.avatar_url;
+      userLogin.textContent = data.login;
+    });
 }
 
-getGitHubProfileInfos()
+getGitHubProfileInfos();
